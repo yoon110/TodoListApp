433eef33e219b0e6feccc7c0bfae6abc39b31d08
yoon110 <22000706@handong.edu>
2021-09-26 23:47:40 +0900

    5.1th commit

--- src/com/todo/menu/Menu.java
+++ src/com/todo/menu/Menu.java
@@ -12,7 +12,7 @@ public class Menu {
         System.out.println("ls_name_asc  - 이름 순 정렬 목록");
         System.out.println("ls_name_desc - 이름 역순 정렬 목록");
         System.out.println("ls_date      - 날짜 순 정렬 목록");
-        System.out.println("ls_date_desc - 날짜 순 정렬 목록");
+        System.out.println("ls_date_desc - 날짜 역순 정렬 목록(최신순)");
         System.out.println("ls_cate      - 카테고리 목록");
         System.out.println("find         - 제목, 내용 내 키워드 검색");
         System.out.println("find_cate    - 카테고리 내 키워드 검색");
